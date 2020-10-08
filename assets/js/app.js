ee1654117133e0d2ef5d089ce50724823f1f1088
sebastien juchet <dyonisos40games@gmail.com>
2020-10-08 12:22:31 +0200

    Ajout de la classe search pour rechercher dans les annonces

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,9 +7,18 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../scss/app.scss';
+import Search from '../js/search';
 
 let picture = document.querySelector('.picture-profil')
 let modal = document.querySelector('.identity')
 picture.addEventListener('click', (e) => {
     modal.classList.toggle('identity-display')
 })
+
+
+if (document.querySelector('#btn-search')) {
+    let btnSearch = document.querySelector('#btn-search')  
+    btnSearch.addEventListener('click', (e) => {
+        new Search('.card-vehicle', '.title-vehicle', '#search-vehicle', '.description-vehicle')
+    })
+}
